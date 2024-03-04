9d664ac58b1070ed0313b354113e7b28ff1767c4
Josias Junior Santos <josiajrsantos@gmail.com>
2024-03-04 00:46:23 -0300

    Inicio do commit

---
+++ tecnicasdeprogramacao/src/main/java/lista2/Ex7.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2024 Josias Junior Santos  <josiajrsantos@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package lista2;
+
+/**
+ *
+ * @author Josias Junior Santos  <josiajrsantos@gmail.com>
+* @date 03/03/2024
+* @brief Class Ex7
+ */
+import java.util.Scanner;
+public class Ex7 {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if ( a > b ){
+            System.out.print("Maior: " + a);
+        } else { 
+            System.out.print("Maior: " + b);
+        }
+    }
+}
